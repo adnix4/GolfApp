0c757b94810fe9f6e630f5b6fa3317ff8d9633c5
adnix4 <anixdorf@student.cvtc.edu>
2026-04-24 10:10:31 -0500

    error fix

--- backend/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb6c4055b77b58e11ff01929fb5bc88cd71e07e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c332e039a48c63b9c1b72e31a1e633ace191eee")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
