97bb2c9d81f243b8e7b73cf2f3c47387aee3db7e
adnix4 <anixdorf@student.cvtc.edu>
2026-05-23 07:20:40 -0500

    Claude updates

--- apps/api-tests/obj/Debug/net8.0/WebAPI.Tests.AssemblyInfo.cs
+++ apps/api-tests/obj/Debug/net8.0/WebAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e015a19673a78f66eda77123bbd36a6d7ce740e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f715ffb6ca1a43203d74a2b9e118933d3c7f2a67")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
